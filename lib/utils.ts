5cb3acd0a0801b82d62e5d9fda892b6d0142dc95
Richard Abishai <rickoshade1891@gmail.com>
2024-11-16 22:03:53 +0530

    Add DocumentView component and sanitizeString utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const sanitizeString = (str: string) => {
+  return str.replace(/[^\x20-\x7E]/g, (char) => {
+    return '';
+  });
+};
