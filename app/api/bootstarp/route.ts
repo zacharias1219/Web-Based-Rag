419ee5aa203025daaff92c18e643a44921d23d1d
Richard Abishai <rickoshade1891@gmail.com>
2024-11-16 22:00:54 +0530

    Refactored the code

--- app/api/bootstarp/route.ts
+++ app/api/bootstarp/route.ts
@@ -1,8 +1,11 @@
-import { initialBootstrapping } from "@/app/services/bootstrap";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { initiateBootstrapping } from '../../services/bootstrap';
+
+export const maxDuration = 300;
+
 
 export async function POST() {
-    await initialBootstrapping(process.env.PINECONE_INDEX as string);
+  await initiateBootstrapping(process.env.PINECONE_INDEX as string)
 
-    return NextResponse.json({success: true}, {status: 200});
+  return NextResponse.json({ success: true }, { status: 200 })
 }
