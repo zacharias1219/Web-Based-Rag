2efef0b2c705b82487773fd25949e015171c9169
Richard Abishai <rickoshade1891@gmail.com>
2024-11-16 22:01:18 +0530

    Refactored the code

--- app/api/ingest/route.ts
+++ app/api/ingest/route.ts
@@ -1,8 +1,13 @@
-import { handleBootstrapping } from "@/app/services/bootstrap";
 import { NextRequest, NextResponse } from "next/server";
 
+import { handleBootstrapping } from "../../services/bootstrap";
+
+export const maxDuration = 300;
+
 export async function POST(req: NextRequest) {
-    const { targetIndex } = await req.json();
-    await handleBootstrapping(targetIndex);
-    return NextResponse.json({ success: true }, { status: 200 });
+  const { targetIndex } = await req.json();
+
+  await handleBootstrapping(targetIndex);
+
+  return NextResponse.json({ success: true }, { status: 200 });
 }
